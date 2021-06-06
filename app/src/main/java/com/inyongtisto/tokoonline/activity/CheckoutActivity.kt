68cf796abe21c27a6de001d5cb355bb149348a06
Putri <aiyukape@gmail.com>
2021-06-06 23:08:09 +0700

    mengubah checkout logic 3

--- app/src/main/java/com/inyongtisto/tokoonline/activity/CheckoutActivity.kt
+++ app/src/main/java/com/inyongtisto/tokoonline/activity/CheckoutActivity.kt
@@ -1,15 +1,20 @@
 package com.inyongtisto.tokoonline.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.inyongtisto.tokoonline.MainActivity
 import com.inyongtisto.tokoonline.R
 import com.inyongtisto.tokoonline.helper.Helper
+import com.inyongtisto.tokoonline.helper.SharedPref
 import com.inyongtisto.tokoonline.room.MyDatabase
 import com.tapadoo.alerter.Alerter
+import kotlinx.android.synthetic.main.activity_list_alamat.*
 import kotlinx.android.synthetic.main.activity_masuk.*
 import kotlinx.android.synthetic.main.activity_pengiriman.*
 
@@ -23,7 +28,7 @@ class CheckoutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_pengiriman)
         myDb = MyDatabase.getInstance(this)!!
 
-        btn_bayar.setOnClickListener{
+        btn_bayar.setOnClickListener {
             Alerter.create(this)
             Alerter.create(this)
                 .setTitle("Yay! Pesananmu sedang kami proses :)")
@@ -31,18 +36,77 @@ class CheckoutActivity : AppCompatActivity() {
                 .setBackgroundColorRes(R.color.colorPrimaryVegan)
                 .setDuration(7000)
                 .setOnClickListener(View.OnClickListener {
-                    Toast.makeText(applicationContext, "Notification clicked", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "Notification clicked", Toast.LENGTH_SHORT)
+                        .show()
 
                 })
-
                 .show()
+            //bayar()
         }
+        totalHarga = Integer.valueOf(intent.getStringExtra("extra")!!)
+        tv_totalBelanja.text = Helper().gantiRupiah(totalHarga)
+        setSpinnerSektor()
+        setSpinnerWaktuKirim()
 
     }
 
+    //spinner sektor kota
+    fun setSpinnerSektor() {
+        val arryString = ArrayList<String>()
+        arryString.add("Tegal")
+        arryString.add("Garut")
+        arryString.add("Purwokerto")
 
+        val adapter = ArrayAdapter<Any>(this, R.layout.item_spinner, arryString.toTypedArray())
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner_kota.adapter = adapter
+        spinner_kota.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
 
+            }
 
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //if (position != 0) {
+                //  getOngkir(spinner_kota.selectedItem.toString())
+            }
+        }
+    }
 
+    //spinner waktu kirim
+    fun setSpinnerWaktuKirim() {
+        val arryString = ArrayList<String>()
+        arryString.add("Pagi Hari (09.00 WIB - 11.00 WIB)")
+        arryString.add("Siang Hari (13.00 WIB - 15.00 WIB)")
+        arryString.add("Sore Hari (16.00 WIB - 17.00 WIB)")
 
+        val adapter = ArrayAdapter<Any>(this, R.layout.item_spinner, arryString.toTypedArray())
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner_waktu_kirim.adapter = adapter
+        spinner_waktu_kirim.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                //if (position != 0) {
+                //  getOngkir(spinner_kota.selectedItem.toString())
+            }
+        }
+    }
 }
+
+
+
+
+
+
