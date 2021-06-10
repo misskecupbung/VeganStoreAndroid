8519c2117cd1ada30dea84928aa11d557bfa0f38
Malihatmalam <malihatmalam@gmail.com>
2021-06-10 21:52:28 +0700

    Revisi Arya Uhuy 2

--- app/src/main/java/com/inyongtisto/tokoonline/model/order_detail.kt
+++ app/src/main/java/com/inyongtisto/tokoonline/model/order_detail.kt
@@ -1,4 +1,13 @@
 package com.inyongtisto.tokoonline.model
 
 class order_detail {
+    var id = 0
+    var product_name : String = ""
+    var price = 0
+    var qty = 0
+    var total = 0
+//    var order_id = 0
+//    var product_id = 9
+//    var created_at = ""
+//    var updated_at = ""
 }
